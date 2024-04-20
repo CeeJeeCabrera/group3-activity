4e6ca6a6b285f0c98d67fa6d38dd779ab748d3b4
Jay Esplaguera <78730-22251706@SERVER2.DOMAIN>
2024-04-20 11:24:20 +0800

    with config

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,9 @@
+function SignIn() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default SignIn
---
+++ src/config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBafqZxXpDmhyQgKJFPBa8BEZBED-IoY8c",
+  authDomain: "group3-activty5-6.firebaseapp.com",
+  databaseURL: "https://group3-activty5-6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "group3-activty5-6",
+  storageBucket: "group3-activty5-6.appspot.com",
+  messagingSenderId: "1065930611412",
+  appId: "1:1065930611412:web:f66a39b4ab75207b4b3941",
+  measurementId: "G-G4E2X9D3E0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
